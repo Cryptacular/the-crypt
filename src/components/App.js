edffbc526a5c0f549c86302f46cbda5ed4715bf4
Nick Mertens <nickemans@gmail.com>
2017-02-06 20:07:05 +1300

    Move Info panel to static HTML. Tweak layout/flexbox styles.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Router from 'react-router-component';
-import Info from './Info';
 import Chat from './Chat';
 import Page from './Page';
 import Empty from './Empty';
@@ -13,18 +12,12 @@ var NotFound = Router.NotFound;
 class App extends Component {
   render() {
     return (
-      <div>
-        <header className="cr-header">
-          <h1 className="cr-header-title">Welcome to The Crypt!</h1>
-        </header>
-        <div className="cr-app">
-          <Info className="cr-app-section cr-app-section--small" />
-          <Chat className="cr-app-section" />
-          <Locations hash component={null}>
-            <Location path="/:pagename" handler={Page} className="cr-app-section" />
-            <NotFound handler={Empty} />
-          </Locations>
-        </div>
+      <div className="cr-app-content">
+        <Chat className="cr-app-section" />
+        <Locations hash component={null}>
+          <Location path="/:pagename" handler={Page} className="cr-app-section" />
+          <NotFound handler={Empty} />
+        </Locations>
       </div>
     );
   }
